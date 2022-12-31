513552483108b342c03f13f51ac93d8f4d01335b
serg <91432388+hiimsergey@users.noreply.github.com>
2022-12-31 21:26:24 +0100

    aur.sh

---
+++ aur.sh
@@ -0,0 +1,13 @@
+#!/usr/bin/sh
+paru -S \
+chromium-extension-ublock-origin \
+chromium-extension-web-store \
+gruvbox-material-gtk-theme-git \
+gruvbox-material-icon-theme-git \
+pfetch \
+picom-jonaburg-git \
+prismlauncher-bin \
+timeshift-bin \
+ttf-whatsapp-emoji \
+ungoogled-chromium \
+xcursor-simp1e-gruvbox-dark
